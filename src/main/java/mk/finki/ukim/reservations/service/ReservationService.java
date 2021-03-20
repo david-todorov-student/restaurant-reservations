a8d088dca2c52a1ae1ad46991ba0bf342efa65ad
bbozilov26 <bobobozilov@gmail.com>
2021-03-20 23:03:12 +0100

    reservation controller adjusted

--- src/main/java/mk/finki/ukim/reservations/service/ReservationService.java
+++ src/main/java/mk/finki/ukim/reservations/service/ReservationService.java
@@ -1,6 +1,7 @@
 package mk.finki.ukim.reservations.service;
 
 import mk.finki.ukim.reservations.model.Reservation;
+import mk.finki.ukim.reservations.model.Restaurant;
 import mk.finki.ukim.reservations.model.Table;
 import mk.finki.ukim.reservations.model.User;
 import org.springframework.stereotype.Service;
@@ -11,9 +12,17 @@ import java.util.List;
 @Service
 public interface ReservationService {
 
-    Reservation makeReservation(Long tableId, LocalDateTime validFrom, LocalDateTime validUntil, Long userId);
+    Reservation makeReservation(Long tableId, LocalDateTime validFrom, LocalDateTime validUntil, User user, List<Restaurant> restaurants);
 
     void removeReservation(Long id);
 
     List<Reservation> listByRestaurant(Long restaurantId);
+
+    List<Reservation> getPlacedReservationsForUser(User user);
+
+    //Reservation placeReservation(User user, List<Reservation> reservations);
+
+    List<Restaurant> getAllRestaurantsInUserActiveReservations(String username);
+
+    void changeStatusToFinishedOfActiveReservationList(String username);
 }
--- src/main/java/mk/finki/ukim/reservations/service/impl/ReservationServiceImpl.java
+++ src/main/java/mk/finki/ukim/reservations/service/impl/ReservationServiceImpl.java
@@ -6,7 +6,6 @@ import mk.finki.ukim.reservations.model.Table;
 import mk.finki.ukim.reservations.model.User;
 import mk.finki.ukim.reservations.model.exceptions.RestaurantNotFoundException;
 import mk.finki.ukim.reservations.model.exceptions.TableNotFoundException;
-import mk.finki.ukim.reservations.model.exceptions.UserNotFoundException;
 import mk.finki.ukim.reservations.repository.ReservationRepository;
 import mk.finki.ukim.reservations.repository.RestaurantRepository;
 import mk.finki.ukim.reservations.repository.TableRepository;
@@ -24,23 +23,32 @@ public class ReservationServiceImpl implements ReservationService {
     private final TableRepository tableRepository;
     private final UserRepository userRepository;
     private final RestaurantRepository restaurantRepository;
+    private final ReservationService reservationService;
 
-    public ReservationServiceImpl(ReservationRepository reservationRepository, TableRepository tableRepository, UserRepository userRepository, RestaurantRepository restaurantRepository) {
+    public ReservationServiceImpl(ReservationRepository reservationRepository,
+                                  TableRepository tableRepository,
+                                  UserRepository userRepository,
+                                  RestaurantRepository restaurantRepository,
+                                  ReservationService reservationService) {
         this.reservationRepository = reservationRepository;
         this.tableRepository = tableRepository;
         this.userRepository = userRepository;
         this.restaurantRepository = restaurantRepository;
+        this.reservationService = reservationService;
     }
 
     @Override
-    public Reservation makeReservation(Long tableId, LocalDateTime validFrom, LocalDateTime validUntil, Long userId) {
+    public Reservation makeReservation(Long tableId, LocalDateTime validFrom, LocalDateTime validUntil, User user, List<Restaurant> restaurants) {
         Table table = this.tableRepository.findById(tableId)
                 .orElseThrow(TableNotFoundException::new);
-        User user = this.userRepository.findById(userId)
-                .orElseThrow(UserNotFoundException::new);
-
+//        User user = this.userRepository.findById(userId)
+//                .orElseThrow(UserNotFoundException::new);
         Reservation reservation = new Reservation(table, validFrom, validUntil, user);
-        return this.reservationRepository.save(reservation);
+
+        this.reservationRepository.save(reservation);
+        this.reservationService.changeStatusToFinishedOfActiveReservationList(user.getUsername());
+
+        return reservation;
     }
 
     @Override
@@ -54,6 +62,28 @@ public class ReservationServiceImpl implements ReservationService {
                 .orElseThrow(RestaurantNotFoundException::new);
 
         return this.reservationRepository.findAllByTableRestaurant(restaurant);
+    }
+
+    @Override
+    public List<Reservation> getPlacedReservationsForUser(User user) {
+        if (user == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return reservationRepository.findAllByUser(user);
+    }
+
+    @Override
+    public List<Restaurant> getAllRestaurantsInUserActiveReservations(String username) {
+        Reservation activeReservation = this.getActiveReservation(username);
+        return activeReservation.geRestaurants();
+    }
+
+    @Override
+    public void changeStatusToFinishedOfActiveReservationList(String username) {
+        Reservation activeReservation = this.getActiveReservation(username);
+        activeReservation.setStatus(ReservationStatus.FINISHED);
+        reservationRepository.save(activeReservation);
     }
 
 
